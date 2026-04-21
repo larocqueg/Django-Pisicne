56c930a2f31c5aa9f8231e2c05aa692214a5dbcb
larocqueg <portugalgabriel2021@gmail.com>
2026-04-21 15:30:30 +0100

    created extra basic tests just to see if i got it

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,5 +4,23 @@ Calculator functions
 
 
 def add(x, y):
-    """Add x and y and return result"""
+    """Add numbers and return result"""
     return x + y
+
+
+def subtract(x, y):
+    """Subtract numbers and return result"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply numbers and return result"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide numbers and return result"""
+    if (y != 0):
+        return x / y
+    print("Dividing by 0 is impossible!\n")
+    return 0
--- app/app/tests.py
+++ app/app/tests.py
@@ -13,5 +13,19 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5, 6)
-
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.subtract(10, 9)
+        self.assertEqual(res, 1)
+
+    def test_multiplicating_numbers(self):
+        """Test multiplicating numbers"""
+        res = calc.multiply(10, 10)
+        self.assertEqual(res, 100)
+
+    def test_dividing_numbers(self):
+        """Test dividing numbers"""
+        res = calc.divide(100, 10)
+        self.assertEqual(res, 10)
